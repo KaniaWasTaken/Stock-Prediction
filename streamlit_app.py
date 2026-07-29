babeb161d61e3631b16262355a2f12f3d6c661d7
KaniaWasTaken <kaniawastaken@gmail.com>
2026-07-29 16:02:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ import streamlit as st
 
 # Add the project root directory to Python's import path.
 # This allows app.py to import modules from the src folder.
-PROJECT_ROOT = Path(__file__).resolve().parent.parent
+PROJECT_ROOT = Path(__file__).resolve().parent
 
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
